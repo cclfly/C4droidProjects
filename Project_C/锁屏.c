3b816dc250eed0058987d5cec9601328c1bbd25b
root <root@localhost>
2017-09-03 11:25:55 +0800

    c

--- Project_C/锁屏.c
+++ Project_C/锁屏.c
@@ -6,10 +6,10 @@ int main(void)
 	char ch;
 	printf("要淡定。。。\n");
 	sleep(1);
-	for (ch = 'a'; ch <= 'z'; ch++)
+	//for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		sleep(1);
 		printf("%c\n", ch);
-//		system("su -u input keyevent 6");
+		system("su -u input keyevent 6");
 	}
 }
