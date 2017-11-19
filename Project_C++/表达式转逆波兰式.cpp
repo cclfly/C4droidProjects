2c13a05b156211a6bef593b3547f9da1a9f213aa
root <root@localhost>
2017-11-19 14:52:09 +0800

    表达式转逆波兰式

---
+++ Project_C++/表达式转逆波兰式.cpp
@@ -0,0 +1,39 @@
+/**
+ * @brief 表达式转逆波兰式
+ * @author cclfly
+ * @version 0.0.1
+ * 11月19日14:51 开坑
+ */
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void func(string str)
+{
+	str += '#';
+	string tmp;
+	stack<string> stk;
+	for (int i=0;i<str.length();i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			tmp += str[i];
+		}
+		else
+		{
+			stk.push(tmp);
+			cout << tmp << endl;
+			tmp.clear();
+		}
+	}
+}
+
+int main()
+{
+	string str;
+	cin>>str;
+	cout << "< " << str;
+	cout << "> " << func(str);
+	return 0;
+}
