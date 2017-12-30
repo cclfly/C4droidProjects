08b72a7181989bd9ac1fc6f7309e8828b7dae3f4
root <root@localhost>
2017-12-30 21:43:10 +0800

    2

--- Project_Qt/123.cpp
+++ Project_Qt/123.cpp
@@ -2,27 +2,18 @@
 #include <QDesktopWidget>
 #include <QWidget>
 #include <QPushButton>
-#include <string>
-#include <sstream>
-
-using std::string;
-using std::stringstream;
 
 int main(int argc,char*argv[])
 {
 	QApplication app(argc,argv);
 	int width=QApplication::desktop()->width();
 	int height=QApplication::desktop()->height();
-	/*stringstream ss;
-	string str;
-	ss<<wid;
-	ss>>str;*/
 	
 	QWidget w(0);
 	QPushButton b(&w);
 	b.setText("exit");
 	b.setGeometry((width-200)/2,(height-80-80)/2,200,80);
-	QWidget::connect(&b,SIGNAL(clicked()),&app,SLOT(quit()));
+	QWidget::connect(&b,&QPushButton::clicked,&app,&QApplication::quit);
 	
 	w.show();
 	return app.exec();
