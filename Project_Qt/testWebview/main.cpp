24c8758bb65319cc9fa90cd32a1034ca7479a28f
root <root@localhost>
2019-07-15 15:41:42 +0800

    c4的qt版本居然是5.11.1

--- Project_Qt/testWebview/main.cpp
+++ Project_Qt/testWebview/main.cpp
@@ -1,10 +1,14 @@
 #include <QApplication>
-#include <QWebview>
+#include <QPushButton>
+//#include <QWebview>
 
 int main(int argc,char*argv[])
 {
 	QApplication app(argc,argv);
-	QWebview web;
-	web.show();
+	//QWebview web;
+	//web.show();
+	QPushButton btn;
+	btn.setText(QString(qVersion()));
+	btn.show();
 	app.exec();
 }
